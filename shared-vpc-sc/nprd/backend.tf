997641e09f57341c30a23e33f09e0d66dace5f9f
Bhupendra Kumar <97610841+Bhupendra789@users.noreply.github.com>
2026-03-06 00:55:40 +0530

    Update backend.tf

--- shared-vpc-sc/nprd/backend.tf
+++ shared-vpc-sc/nprd/backend.tf
@@ -1,1 +1,10 @@
+terraform {
+  backend "remote" {
+    hostname     = "prd-tfe-foundation.richemont.com"
+    organization = "Richemont-International-SA"
 
+    workspaces {
+      name = "gcp-shared-nprd-sc-perimeter"
+    }
+  }
+}
